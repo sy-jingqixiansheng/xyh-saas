e0adaa24d6710482b4c91e84e1b218203f51aad4
shaoqi <77123941@qq.com>
2020-08-23 16:32:36 +0800

    添加add

---
+++ xyh_common/src/main/java/com/xyh/common/entity/resqonse/demo.java
@@ -0,0 +1,12 @@
+package com.xyh.common.entity.resqonse;
+
+/**
+ * @Author SQ
+ * @Date 2020/8/23 0023 16:31
+ * @Version 1.0
+ */
+public class demo {
+    public static void main(String[] args) {
+        System.out.println("asdsads");
+    }
+}
